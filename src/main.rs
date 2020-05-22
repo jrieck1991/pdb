f5f52628ef8c3e1d6fc6fb6bc0e9fb6a4f7eb94d
Jack Rieck <jack.rieck@sendgrid.com>
2020-05-22 12:11:05 -0600

    add print

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ fn main() {
 
     // seal
     let label: [u8; 16] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+    println!("label {:?}", label);
     let (cipher_text, seal_data) = enclave::seal_key(label);
     println!("cipher_text: {:?}\n seal_data: {:?}", cipher_text, seal_data);
 
