e722cbdf9361c6b13b0b450de69e30c064957e44
Jack Rieck <jack.rieck@sendgrid.com>
2020-05-25 19:37:14 -0600

    builds with unseal, time to try

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,27 @@ mod enclave;
 mod encrypt;
 
 fn main() {
-    // get mrenclave sealing key
+    // arbitrary label to apply with sealing key, store
     let label: [u8; 16] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
-    let (seal_key, _seal_data) = enclave::seal_key(label);
+
+    // get mrenclave sealing key and seal data
+    // store both
+    let (seal_key, seal_data) = enclave::seal_key(label);
 
     // TODO: will panic if plaintext ln is not a multiple of 16 bytes
     let mut plain_text = String::from("jdhgywiqlakdlokj").into_bytes();
 
     // encrypt data
-    let mut _encrypted = encrypt::encrypt(&seal_key, &mut plain_text);
+    let mut encrypted = encrypt::encrypt(&seal_key, &mut plain_text);
 
     // delete key from memory to prevent leaks
     // save data
 
-    // use same key to decrypt
-    //let mut plain = encrypt::decrypt(&key, &mut encrypted);
+    // get unseal key
+    let unseal_key = enclave::unseal_key(label, seal_data).unwrap();
 
-    // unseal
+    // use unseal key to decrypt
+    let plain_res = encrypt::decrypt(&unseal_key, &mut encrypted);
+
+    println!("result: {}", String::from_utf8(plain_res).unwrap());
 }
