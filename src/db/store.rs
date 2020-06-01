624ac14cd49ebbad4f2669f58ea4c2c8be4a7e8a
jrieck <jack.rieck@sendgrid.com>
2020-06-01 16:27:39 -0600

    Store (#2)
    * store data
    
    * stub out storage client
    
    * switch to rocksdb
    
    * added mock for enclave to make local testing easier
    
    * get and put compiling
    
    * newline
    
    * store might be working, time to try
    
    * got the layer working
    
    * cargo fix
    
    * mock returns val
    
    * fmt
    
    * unseal key drop

---
+++ src/db/store.rs
@@ -0,0 +1,27 @@
+use rocksdb::DB;
+
+pub struct Client {
+    storage: DB,
+}
+
+impl Client {
+    pub fn new(path: &str) -> Client {
+        let db = DB::open_default(path).unwrap();
+
+        Client { storage: db }
+    }
+
+    pub fn put(&self, key: &[u8], value: &[u8]) {
+        self.storage.put(key, value);
+    }
+
+    pub fn get(&self, key: &[u8]) -> Option<Vec<u8>> {
+        match self.storage.get(key) {
+            Ok(opt_value) => match opt_value {
+                Some(value) => Some(value.to_vec()),
+                None => None,
+            },
+            Err(_e) => None,
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,21 @@
-mod enclave;
-mod encrypt;
+mod db;
 
 fn main() {
-    // arbitrary label to apply with sealing key, store
-    let label: [u8; 16] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+    // init db client
+    let client = db::DB::new("/tmp/storage_path");
 
-    // get mrenclave sealing key and seal data
-    // store both
-    let (seal_key, seal_data) = enclave::seal_key(label);
+    // TODO: will panic if key not in chunks of 16
+    let key = String::from("jdhgywiqlakdlokj").into_bytes();
 
-    // TODO: will panic if plaintext ln is not a multiple of 16 bytes
-    let mut plain_text = String::from("jdhgywiqlakdlokj").into_bytes();
+    // TODO: will panic if value not in chunks of 16
+    let value = String::from("jdhgydjalakdloke").into_bytes();
 
-    // encrypt data
-    let mut encrypted = encrypt::encrypt(&seal_key, &mut plain_text);
+    // store data
+    client.put(&key, &value);
 
-    // delete key from memory to prevent leaks
-    // save data
-
-    // get unseal key
-    let unseal_key = enclave::unseal_key(label, seal_data).unwrap();
-
-    // use unseal key to decrypt
-    let plain_res = encrypt::decrypt(&unseal_key, &mut encrypted);
-
-    println!("result: {}", String::from_utf8(plain_res).unwrap());
+    // store encrypted data and seal_data
+    match client.get(&key) {
+        Some(val) => println!("value: {}", String::from_utf8(val).unwrap()),
+        None => println!("no item found for key"),
+    };
 }
