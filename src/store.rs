620a92ff20e45c17857fab98799e3131d9b8d4e0
Jack Rieck <jack.rieck@sendgrid.com>
2020-06-01 15:13:02 -0600

    get and put compiling

--- src/store.rs
+++ src/store.rs
@@ -16,8 +16,16 @@ impl Client {
     }
 
     pub fn put(&self, key: &str, value: &str) {
+        self.db.put(key, value);
     }
 
-    pub fn get(&self, key: &str){
+    pub fn get(&self, key: &str) -> Option<String> {
+        match self.db.get(key) {
+            Ok(opt_value) => match opt_value {
+                Some(value) => Some(String::from_utf8(value).unwrap()),
+                None => None,
+            } 
+            Err(e) => None,
+        }
     }
 }
