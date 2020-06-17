108b46b6798e3d9f9c39e9cae520dc7ca445b8aa
Jack Rieck <jack.rieck@sendgrid.com>
2020-06-17 13:38:52 -0400

    abstract store and listen for incoming requests

--- trusted/src/main.rs
+++ trusted/src/main.rs
@@ -4,20 +4,6 @@ fn main() {
     // init client and connect to database
     let mut client = trusted::Client::new("localhost:9998", "localhost:9999");
 
-    // TODO: will panic if key not in chunks of 16
-    let key = String::from("jdhgywiqlakdlokj").into_bytes();
-
-    // TODO: will panic if value not in chunks of 16
-    let value = String::from("jdhgydjalakdloke").into_bytes();
-
-    // store data
-    println!("put value");
-    client.put(&key, &value);
-
-    // get value
-    println!("get value");
-    match client.get(&key) {
-        Some(value) => println!("value: {:?}", value),
-        None => println!("no match found for key: {:?}", key),
-    };
+    // handle incoming requests
+    client.handle();
 }
